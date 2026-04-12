028bf05bf30f4507fdbc84ac55ff9a7c91b22f00
Pranay Tamada <pranay.tamada@gmail.com>
2026-04-12 13:57:35 +0530

    Fix: Resolve Vercel 404 by refining routing in vercel.json and server.js

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
   const user = USERS.find(u => u.username === username && u.password === password);
